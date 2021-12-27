9825fd818f6f30b8517208c0dc187504522081ed
Михаил Комогорцев <mih.kom1998@gmail.com>
2021-12-27 23:05:46 +0300

    PrintArray Method added

--- C#_Task/Program.cs
+++ C#_Task/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Написать программу, которая из имеющегося массива целых чисел формирует
+массив из четных чисел.Первоначальный массив можно ввести с клавиатуры,
+либо сгенерировать случайным образом. При решении не рекомендуется
+пользоваться коллекциями, лучше обойтись исключительно массивами.*/
+
+
+int[] numbers = new int[] { 1, 5, 6, 8, 9 };
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.WriteLine();
+}
+PrintArray(numbers);
+
+int FindEven(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+    }
+}
