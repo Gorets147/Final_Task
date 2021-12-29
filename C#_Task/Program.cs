dc9cf7235625e0b473187aa24115ce18478b15ee
Михаил Комогорцев <mih.kom1998@gmail.com>
2021-12-29 22:43:40 +0300

    FindEven Function added

--- C#_Task/Program.cs
+++ C#_Task/Program.cs
@@ -4,7 +4,8 @@
 пользоваться коллекциями, лучше обойтись исключительно массивами.*/
 
 
-int[] numbers = new int[] { 1, 5, 6, 8, 9 };
+int[] numbers = new int[] { 2, 3, 5, 3, 8, 4,10,12 };
+int[] EvenArray = new int[1];
 
 void PrintArray(int[] array)
 {
@@ -14,12 +15,36 @@ void PrintArray(int[] array)
     }
     System.Console.WriteLine();
 }
-PrintArray(numbers);
-
-int FindEven(int[] array)
+int[] FindEven(int[] array)
 {
+    int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        
+        if (array[i] % 2 == 0)
+        {
+            count = count + 1;
+        }
+        else
+        {
+            System.Console.WriteLine($"Число {array[i]} - не чётное");
+        }
     }
+    System.Console.WriteLine(count + "- Кол-во чётных чисел");
+    EvenArray = new int[count];
+    int j = 0;
+    int t = 0;
+    while (j < EvenArray.Length)
+    {
+        if (array[t] % 2 == 0)
+        {
+            EvenArray[j] = array[t];
+            j++;
+        }
+        t++;
+    }
+    return EvenArray;
 }
+
+PrintArray(EvenArray);
+FindEven(numbers);
+PrintArray(EvenArray);
